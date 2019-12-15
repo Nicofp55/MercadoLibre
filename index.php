0d8e4487a5797913a614afbaa6978e71f989c511
Nicofp55 <nicolasfperez55@gmail.com>
2019-12-15 18:07:48 -0300

    Final Version

--- index.php
+++ index.php
@@ -23,12 +23,14 @@
 	// descargarlo (pasar link).
 	// ● Debe tener documentación explicando como usarlo.
 	// ● Subir el file de output generado por el script
-	function fetch($user_id, $site_id, $filename){
 
+	// Declaración de Funciones
+	function fetch($user_id, $site_id){
+		$today = date('Y-m-d');
 		$json = file_get_contents('https://api.mercadolibre.com/sites/'.$site_id.'/search?seller_id='.$user_id);
 		$categories = file_get_contents('https://api.mercadolibre.com/sites/'.$site_id.'/categories');
 		$categories = json_decode($categories, TRUE);
-		$log = fopen($filename, "w");
+		$log = fopen('.\\logs\\'.$today.'-articles-'.$user_id.'.log', "a+");
 		$json = json_decode($json, TRUE);
 		$text = "----- START OF USER_ID : ".$user_id." DATA ------ \n";
 		
@@ -44,16 +46,24 @@
 		$text .= "----------- END OF USER_ID: ".$user_id." DATA ---------- \n";
 		fwrite($log, $text);
 	}
-	function search($user_id,  $site_id, $filename){
+
+	function search($user_id,  $site_id){
 		
-		if(is_array($user_id)){
-			foreach($user_id as $user){
-				fetch($user, $site_id, $filename);
+		if(is_array($user_id['u'])){
+			foreach($user_id['u'] as $user){
+				fetch($user, $site_id);
 			}
 		}else{
-			fetch($user_id, $site_id, $filename);
+			fetch($user_id['u'], $site_id);
 		}
 		
 		
 	}
-	search('179571326', 'MLA','./logs/articles.log');
+
+	//LLamado de Funciones
+	$users = getopt("u:");
+	if($users){
+		search($users, 'MLA');	
+	}else{
+		search('179571326', 'MLA');
+	}
